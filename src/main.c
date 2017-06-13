602d389c86511c735f973a049b91f998bcd77a3b
Kirill <desving12345@gmail.com>
2017-06-13 21:40:57 +0700

    Added resolve of quadratic equation

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <functions.h>
+
+int main()
+{
+	float a, b, c, d, x1, x2;
+	printf("%s", "Введите a\n");
+	scanf("%f", &a);
+	printf("%s", "Введите b\n");
+	scanf("%f", &b);
+	printf("%s", "Введите c\n");
+	scanf("%f", &c);
+
+	d = D(a, b, c);
+
+	if (d < 0)
+	{
+		printf("Комплексные корни\n");
+	}
+	else if (d == 0)
+	{
+		printf("Один корень\n");
+		x1 = X(d, a, b, 1);
+		printf("\n%f", x1);
+	}
+	else
+	{
+		printf("Два корня\n");
+		x1 = X(d, a, b, 1);
+		x2 = X(d, a, b, -1);
+		printf("\n%f", x1);
+		printf("\n%f", x2);
+	}
+
+	return 0;
+}
